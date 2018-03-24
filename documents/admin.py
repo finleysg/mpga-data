e7128aa4ae2d369fbcfa48495bc274d4d2b7ecd3
Stuart Finley <finleysg@gmail.com>
2018-03-24 16:45:31 -0500

    image model and event model

--- documents/admin.py
+++ documents/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
-from .models import Document, Tag, DocumentTag
+from .models import Document, Tag, DocumentTag, PhotoTag, Photo, Event
 
 
 class TagInline(admin.TabularInline):
     model = DocumentTag
     can_delete = True
     extra = 3
+
+
+class PhotoTagInline(admin.TabularInline):
+    model = PhotoTag
+    can_delete = True
+    extra = 3
+
+
+class EventAdmin(admin.ModelAdmin):
+    fields = ['year', 'month', 'name', 'location', 'portal_url', ]
+    list_display = ['year', 'name', 'location', ]
+    list_filter = ('year', )
+    save_on_top = True
 
 
 class DocumentAdmin(admin.ModelAdmin):
@@ -22,14 +35,15 @@ class TagAdmin(admin.ModelAdmin):
     list_display = ['name', ]
 
 
-# class PhotoAdmin(admin.ModelAdmin):
-#     fields = ['year', 'photo_type', 'title', 'file', ]
-#     inlines = [TageInline, ]
-#     list_display = ['year', 'title', 'photo_type', ]
-#     list_filter = ('year', 'photo_type', )
-#     save_on_top = True
+class PhotoAdmin(admin.ModelAdmin):
+    fields = ['year', 'photo_type', 'title', 'raw_image', ]
+    inlines = [PhotoTagInline, ]
+    list_display = ['year', 'title', 'photo_type', ]
+    list_filter = ('year', 'photo_type', )
+    save_on_top = True
 
 
 admin.site.register(Document, DocumentAdmin)
 admin.site.register(Tag, TagAdmin)
-# admin.site.register(Photo, PhotoAdmin)
+admin.site.register(Photo, PhotoAdmin)
+admin.site.register(Event, EventAdmin)
--- documents/models.py
+++ documents/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from imagekit import ImageSpec
+from imagekit.models import ImageSpecField
+from pilkit.processors import ResizeToFit
+
+from clubs.models import GolfCourse
 
 DOCUMENT_TYPE_CHOICES = (
     ("EA", "Event Application"),
@@ -12,12 +17,12 @@ DOCUMENT_TYPE_CHOICES = (
     ("O", "Other")
 )
 
-# PHOTO_TYPE_CHOICES = (
-#     ("GC", "Golf Course Photos"),
-#     ("EW", "Event Winners"),
-#     ("EP", "Event Photos"),
-#     ("O", "Other")
-# )
+PHOTO_TYPE_CHOICES = (
+    ("GC", "Golf Course Photos"),
+    ("EW", "Event Winners"),
+    ("EP", "Event Photos"),
+    ("O", "Other")
+)
 
 
 def document_directory_path(instance, filename):
@@ -25,9 +30,32 @@ def document_directory_path(instance, filename):
     return "documents/{0}/{1}".format(instance.year, filename)
 
 
-# def photo_directory_path(instance, filename):
-#     # file will be uploaded to MEDIA_ROOT/documents/year/<filename>
-#     return "photos/{0}/{1}".format(instance.year, filename)
+def photo_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/documents/year/<filename>
+    return "photos/{0}/{1}".format(instance.year, filename)
+
+
+class ThumbnailSpec(ImageSpec):
+    format = 'JPEG'
+    options = {'quality': 70}
+    processors = [ResizeToFit(96, 96)]
+
+
+class WebSpec(ImageSpec):
+    format = 'JPEG'
+    options = {'quality': 80}
+    processors = [ResizeToFit(600, 600)]
+
+
+class Event(models.Model):
+    year = models.IntegerField(verbose_name="Golf Season", blank=True, null=True)
+    month = models.CharField(verbose_name="Month", max_length=12)
+    name = models.CharField(verbose_name="Name", max_length=100)
+    location = models.ForeignKey(verbose_name="Location", to=GolfCourse, blank=True, null=True)
+    portal_url = models.CharField(verbose_name="Portal URL", max_length=200, blank=True)
+
+    def __str__(self):
+        return "{} {}".format(self.year, self.name)
 
 
 class Tag(models.Model):
@@ -41,7 +69,7 @@ class Document(models.Model):
     document_type = models.CharField(verbose_name="Document Type", choices=DOCUMENT_TYPE_CHOICES, max_length=2)
     year = models.IntegerField(verbose_name="Golf Season", blank=True, null=True)
     title = models.CharField(verbose_name="Title", max_length=120)
-    event = models.CharField(verbose_name="Event", max_length=120, blank=True)
+    event = models.ForeignKey(verbose_name="Event", to=Event, null=True, blank=True)
     file = models.FileField(verbose_name="File", upload_to=document_directory_path)
     last_update = models.DateTimeField(auto_now=True)
 
@@ -54,14 +82,20 @@ class DocumentTag(models.Model):
     tag = models.ForeignKey(verbose_name="Tag", to=Tag)
 
 
-# class Photo(models.Model):
-#     photo_type = models.CharField(verbose_name="Type", choices=PHOTO_TYPE_CHOICES, max_length=2)
-#     year = models.IntegerField(verbose_name="Golf Season", default=0)
-#     title = models.CharField(verbose_name="Title", max_length=120)
-#     event = models.CharField(verbose_name="Event", max_length=120, blank=True)
-#     file = models.ImageField(verbose_name="Image", upload_to=photo_directory_path)
-#     last_update = models.DateTimeField(auto_now=True)
-#     tags = models.ManyToManyField(Tag)
-#
-#     def __str__(self):
-#         return self.title
+class Photo(models.Model):
+    photo_type = models.CharField(verbose_name="Type", choices=PHOTO_TYPE_CHOICES, max_length=2)
+    year = models.IntegerField(verbose_name="Golf Season", default=0)
+    title = models.CharField(verbose_name="Title", max_length=120)
+    event = models.ForeignKey(verbose_name="Event", to=Event, null=True, blank=True)
+    raw_image = models.ImageField(verbose_name="Image", upload_to=photo_directory_path)
+    thumbnail_image = ImageSpecField(source="raw_image", id="web:image:thumbnail_image")
+    web_image = ImageSpecField(source="raw_image", id="web:image:web_image")
+    last_update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class PhotoTag(models.Model):
+    document = models.ForeignKey(verbose_name="Photo", to=Photo)
+    tag = models.ForeignKey(verbose_name="Tag", to=Tag)
