f43bac1d1b091d1c293b2af828360f3103ce82b1
Stuart Finley <finleysg@gmail.com>
2025-01-01 13:31:05 -0600

    No longer need workaround for custom storages.

--- custom_storages.py
+++ custom_storages.py
@@ -1,34 +1,10 @@
 from django.conf import settings
+from storages.backends.s3 import S3StaticStorage
 
-from CustomS3Boto3Storage import CustomS3Boto3Storage
 
-
-class StaticStorage(CustomS3Boto3Storage):
+class StaticStorage(S3StaticStorage):
     location = settings.STATICFILES_LOCATION
 
 
-class MediaStorage(CustomS3Boto3Storage):
+class MediaStorage(S3StaticStorage):
     location = settings.MEDIAFILES_LOCATION
-
-    # https://github.com/jschneier/django-storages/issues/382#issuecomment-377174808
-    # def _save_content(self, obj, content, parameters):
-    #
-    #     """
-    #     We create a clone of the content file as when this is passed to boto3 it wrongly closes
-    #     the file upon upload where as the storage backend expects it to still be open
-    #     """
-    #     # Seek our content back to the start
-    #     content.seek(0, os.SEEK_SET)
-    #
-    #     # Create a temporary file that will write to disk after a specified size
-    #     content_autoclose = SpooledTemporaryFile()
-    #
-    #     # Write our original content into our copy that will be closed by boto3
-    #     content_autoclose.write(content.read())
-    #
-    #     # Upload the object which will auto close the content_autoclose instance
-    #     super(MediaStorage, self)._save_content(obj, content_autoclose, parameters)
-    #
-    #     # Cleanup if this is fixed upstream our duplicate should always close
-    #     if not content_autoclose.closed:
-    #         content_autoclose.close()
